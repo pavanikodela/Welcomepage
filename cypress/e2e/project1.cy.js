9037dabf2139971d9a2cd150ab7a6d5973fefd6a
Pavani Kodela <pavanikodela2005@gmail.com>
2025-07-17 13:52:00 +0530

    Update Welcome repo with new code

---
+++ cypress/e2e/project1.cy.js
@@ -0,0 +1,39 @@
+describe('Ebank Login pages', () => {
+
+  beforeEach(() => {
+    cy.LoginPage();
+  });
+
+  it('Test login with empty fields', () => {
+    cy.get('.login-button').click();
+    cy.verifyError('Invalid user ID'); 
+  });
+
+  it('Test login with empty User ID', () => {
+    cy.LoginCredentials(null, '231225');
+    cy.verifyError('Invalid user ID'); 
+  });
+
+  it('Test login with empty PIN', () => {
+    cy.LoginCredentials('142420', null);
+    cy.verifyError('Invalid PIN'); 
+  });
+
+  it('Test login with invalid PIN', () => {
+    cy.LoginCredentials('142420', '123456');
+    cy.verifyError("User ID and PIN didn't match");
+  });
+
+  it('Test login with valid credentials', () => {
+    cy.LoginCredentials('142420', '231225');
+    cy.url().should('eq', 'https://qaebank.ccbp.tech/');
+  });
+
+  it('Test logout functionality', () => {
+    cy.LoginCredentials('142420', '231225');
+    cy.url().should('eq', 'https://qaebank.ccbp.tech/');
+    cy.logout();
+    cy.url().should('include', '/ebank/login'); 
+  });
+
+});
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,36 +1,20 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+Cypress.Commands.add('LoginPage', () => {
+  cy.visit('https://qaebank.ccbp.tech/ebank/login');
+});
 
+Cypress.Commands.add('LoginCredentials', (userId, pin) => {
+  if (userId) cy.get('#userIdInput').type(userId);
+  if (pin) cy.get('#pinInput').type(pin);
+  cy.get('.login-button').click();
+});
 
+Cypress.Commands.add('verifyError', (errorMsg) => {
+  cy.get('.error-message-text').should((element) => {
+    const actualText = element.text().trim().toLowerCase();
+    expect(actualText).to.include(errorMsg.toLowerCase()); 
+  });
+});
 
-
-
-Cypress.Commands.add('ParaBankLoginPage', (Username, Passwprd) => {
-    cy.visit('https://parabank.parasoft.com/parabank')
-    cy.get('input[type="text"]').type('john')
-    cy.get('input[type="password"]').type('demo') 
-    cy.get('input[type="submit"]').click()
-}) 
+Cypress.Commands.add('logout', () => {
+  cy.get('button[type="button"]').click(); 
+});
